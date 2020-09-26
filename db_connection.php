b793a272ac52561f2f4db61fd1b679070b334176
Sai-Yan-Naing <58454014+Sai-Yan-Naing@users.noreply.github.com>
2020-09-26 10:08:55 +0630

    first commit

---
+++ db_connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "plan";
+$password = "welcome123";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>
